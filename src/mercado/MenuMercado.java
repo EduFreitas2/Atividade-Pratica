e42d7f4f0a2fe66b4564213e29f9e6dd38865695
EduFreitas2 <efnascimento.filho2@gmail.com>
2023-12-22 14:13:55 -0300

    MenuLoja

--- src/mercado/MenuMercado.java
+++ src/mercado/MenuMercado.java
@@ -1,6 +1,10 @@
 package mercado;
-
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import java.lang.Thread;
+
 
 public class MenuMercado {
 
@@ -8,74 +12,156 @@ public class MenuMercado {
 		// TODO Auto-generated method stub
 		//instanciando a classe
 				Scanner read = new Scanner(System.in);
-				int opcao;
-				String nome, nomeProduto;
+				int opcao, nivel, tipo, id;
+				String nomePokemon, nomeTreinador, continua;
 				float valor;
 				
 				
-				System.out.println("                MERCADO 71                   ");
+				System.out.println("                MERCADO POKEMON              ");
 				System.out.println("                                             ");
 				System.out.println("*********************************************");
-				System.out.println("Identifique-se\n\n            	                             ");
+				System.out.println("Identifique-se\n\n            	             ");
 				System.out.println("Informe o seu nome: ");
-				nome = read.nextLine();
-				
+				nomeTreinador = read.nextLine();
+				while(true){
+					opcao = 0;
 				System.out.println("\n\n\n*********************************************");
-				System.out.println(nome + ", Bem vindo ao Mercado 71           	 ");
+				System.out.println(nomeTreinador + ", Bem vindo ao Mercado Pokemon");
+				
+				System.out.println("\nAqui nós vendemos e compramos Pokemóns     ");
+				
 				System.out.println("                                             ");
-				System.out.println("         1 - Visualizar Todos os Produtos    ");
-				System.out.println("         2 - Consultar Produto (por número)  ");
-				System.out.println("         3 - Cadastrar Produto               ");
-				System.out.println("         4 - Consultar Saldo                 ");
-				System.out.println("         5 - Depositar                       ");
-				System.out.println("         6 - Comprar Produto                 ");
-				System.out.println("         7 - Sair                            ");
+				System.out.println("         1 - Visualizar Pokémons             ");
+				System.out.println("         2 - Consultar Pokémon               ");
+				System.out.println("         3 - Vender seu Pokémon              ");
+				System.out.println("         4 - Comprar Pokémon                 ");
+				System.out.println("         5 - Histórico de Compras            ");
+				System.out.println("         6 - Sair                            ");
 				System.out.println("*********************************************");
 				System.out.println("Entre com a opção desejada:                  ");
-				do {
-				opcao = read.nextInt();
-				if (opcao > 7 || opcao < 1) {
-					System.out.println("Opção Inválida!");
-					System.out.println("Digite novamente uma opção: ");
-					nome = read.nextLine();
+				try {
+					opcao = read.nextInt();
+				}catch(InputMismatchException e) {
+				read.nextLine();
+				opcao = 0;
 				}
-				}while(opcao > 7 || opcao < 1);
-				
-				switch(opcao)
-				{
-				case 1 ->{
-					System.out.println("Lista de produtos: \n");
-				}
-				case 2->{
-					System.out.println("Digite o Número do Produto: \n");
-				}
-				case 3 ->{
-					System.out.println("Digite o Nome do Produto \n");
-					System.out.println("Digite o valor: \n");
-					
-				}
-				case 4 ->{
-					System.out.println("Consultando saldo... \n");
-				}
-				case 5 ->{
-					System.out.println("Digite o valor que deseja depositar: \n");
-				}
-				case 6 ->{
-					System.out.println("Digite o número do produto: \n");
-				}
-				case 7 ->{
-					System.out.println("**********FIM DO PROGRAMA***********: \n");
+				if (opcao == 6) {
+					read.close();
 					Sobre();
 					System.exit(0);
 				}
+				//opcoes do Crud
+				switch(opcao)
+				{
+				case 1 ->{
+					//exibir todos os pokemons da loja
+					System.out.println("Lista de Pokémons da loja: \n");
+					keyPress();
 				}
+				case 2->{
+					//consultando pokemon
+					System.out.println("Digite o nome do Pokémon: \n");
+				}
+				case 3 ->{
+					//vendendo pokemon
+					System.out.println("Qual pokémon deseja vender \n");
+					read.skip("\\R?");
+					nomePokemon = read.nextLine();
+					
+					System.out.println("Qual é o nível dele? \n");
+					nivel = read.nextInt();
+					System.out.println("Por quanto deseja vender ele? (R$) \n");
+					valor = read.nextFloat();
+					
+					System.out.println("Informe o número do tipo do seu Pokémon: \n");
+					
+					System.out.println("1 - FOGO");
+					System.out.println("2 - ÁGUA");
+					System.out.println("3 - PLANTA");
+					System.out.println("4 - TERRA");
+					do {
+						System.out.println("Entre com a opção desejada");
+						tipo = read.nextInt();
+						if(tipo < 1 || tipo > 4 ) {
+							System.out.println("Valor inválido!");
+						}
+					}while(tipo < 1 || tipo > 4);
+					//novo switch para cadastrar os pokemons em uma das classes filhas PokemonFogo, PokemonAgua, PokemonPlanta, PokemonTerra
+						switch (tipo)
+						{
+						case 1 ->{
+							System.out.println("Verificando se o preço do seu Pokemon é compátivel com o mercado...");
+							try {
+								Thread.sleep(2000);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							keyPress();
+							}
+							case 2 -> {
+								System.out.println("Verificando se o preço do seu Pokemon é compátivel com o mercado...");
+								try {
+									Thread.sleep(2000);
+								} catch (InterruptedException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+								keyPress();
+								
+							}	
+							case 3 ->{
+								System.out.println("Verificando se o preço do seu Pokemon é compátivel com o mercado...");
+								try {
+									Thread.sleep(2000);
+								} catch (InterruptedException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+								keyPress();
+							}
+							case 4 ->{System.out.println("Verificando se o preço do seu Pokemon é compátivel com o mercado...");
+							try {
+								Thread.sleep(2000);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							keyPress();}
+							
+						}
+						
+						}
+				
+				case 4 ->{
+					//consultando o Saldo
+					System.out.println("Consultando saldo... \n");
+					keyPress();
+				}
+				case 5 ->{
+					//valor de deposito
+					System.out.println("Digite o valor que deseja depositar: \n");
+					keyPress();
+				}
+				case 6 ->{
+					
+					System.out.println("Digite o número do produto: \n");
+					keyPress();
+				}
+				default ->{
+					System.out.println("Opcao Inválida \n");
+					keyPress();
+				}
+				
+			}
+			
+		}
 				
 		
 				
-				
-				
-
-	}
+								
+}
+	
 	public static void Sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: Eduardo Freitas ");
@@ -83,5 +169,17 @@ public class MenuMercado {
 		System.out.println("https://github.com/EduFreitas2");
 		System.out.println("*********************************************************");
 	}	
+	public static void keyPress() {
+
+		try {
+
+			System.out.println("\n\nPressione Enter para Continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+
+		}
+	}
 
 }
